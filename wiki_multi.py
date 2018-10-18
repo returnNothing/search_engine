ee07b616e58422ac257560bb0c4c93004f2a752d
returnNothing <44209042+returnNothing@users.noreply.github.com>
2018-10-18 22:05:49 +0300

    Add files via upload

--- wiki_multi.py
+++ wiki_multi.py
@@ -8,19 +8,19 @@ stack = [site.Categories['Русские существительные']]
 
 def check_cat(page, pages, stack):
 
-	# Write down all the nouns in the file.
-	with open('nouns.txt', 'w', encoding='utf_8') as f:
-		if page.namespace != 14:
-				pages.append(page)
-				f.write(page.name + '\n')
-		else:
-			stack.insert(0, page)
+    # Write down all the nouns in the file.
+    with open('nouns.txt', 'w', encoding='utf_8') as f:
+        if page.namespace != 14:
+        pages.append(page)
+        f.write(page.name + '\n')
+    else:
+        stack.insert(0, page)
 
     return pages
-    
+
 while stack:
     cur_node = stack[0]
-    stack = stack[1:]       
+    stack = stack[1:]
     for page in cur_node:
         process = Process(target=check_cat, args=(page, pages, stack))
         process.start()
