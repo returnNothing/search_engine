9f0e23ee466a68c92d38443eb6042142f78d41c1
returnNothing <44209042+returnNothing@users.noreply.github.com>
2018-11-06 20:21:24 +0300

    Add files via upload

--- wiki.py
+++ wiki.py
@@ -4,21 +4,31 @@ to get all Russian nouns.
 """
 
 import mwclient
+from time import time
+
+start_time = time()
 
 # Сonnect to the site.
 site = mwclient.Site('ru.wiktionary.org')
-
-# A list for found nouns (~pages in wictionary). 
-pages = []
 
 # A list used for Depth-first traversal. 
 # Now it contains only a root. 
 stack = [site.Categories['Русские существительные']]
 
+# To check categories and pages we've visited;
+# a list of categories not to traverse.  
+bad_stack = set(['Существительные в винительном падеже',
+             'Существительные в дательном падеже',
+             'Существительные в звательном падеже',
+             'Существительные в предложном падеже',
+             'Существительные в разделительном падеже',
+             'Существительные в родительном падеже',
+             'Существительные в творительном падеже'])
+
 # Write down all the nouns in the file.
 with open('nouns.txt', 'w', encoding='utf_8') as f:
     
-    # Traversing the while tree. 
+    # Traversing the whole tree. 
     while stack:
         
         # Take zero node. 
@@ -27,17 +37,30 @@ with open('nouns.txt', 'w', encoding='utf_8') as f:
         # Iterate through zero node. 
         for page in cur_node:
             
-            # If it's not a category,
-            # and it's not already in the list,
-            # then add to the final list and write down to the file.
-            if page.namespace != 14 and page not in pages:
-                pages.append(page)
-                f.write(page.name + '\n')
-            
-            # Otherwise take it as zero node and iterate. 
+            # If it's not already in the list,
+            # and it's not a category,
+            # then add to the final list, write down to the file
+            # and remember that we've visited the page.
+            if page.name not in bad_stack:
+                if page.namespace != 14:
+                    f.write(page.name + '\n')
+                    bad_stack.add(page.name)
+
+                else:
+                
+                    # In it's a category, take it as zero node and iterate.
+                    stack.insert(0, page)
+                    
+                    # Add category (maybe page) in the set
+                    # to remember that we've visited it already.
+                    bad_stack.add(cur_node.name)
+
+            # If a page has been visited or shouldn't be traversed,
+            # take the next one. 
             else:
-                stack.insert(0, page)
+                continue
         
-
-        
-
+end_time = time()
+time_taken = end_time - start_time
+print("%s minutes" % (round((time_taken / 60), 2)))
+# 7.03 minutes. 
